f99c5a6480f093eeb77f8b4b554a39b6ef3346c8
Kieran <mayork383@gmail.com>
2023-05-23 02:09:01 +0800

    Express and Socket setup

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  newMessage: string = "";
+  messageList: string[] = [];
+
+  constructor(private chatService: ChatService) {
+
+  }
+
+  ngOnInit() {
+    this.chatService.getNewMessage().subscribe((message: string) => {
+      this.messageList.push(message);
+    })
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.newMessage);
+    this.newMessage = '';
+  }
+}
